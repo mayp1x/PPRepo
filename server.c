21c02f477c9dc3133640ba91bc6cdadbcd70b754
mayp1x <46577750+mayp1x@users.noreply.github.com>
2020-05-06 21:19:14 +0200

    Add files via upload

--- server.c
+++ server.c
@@ -45,7 +45,7 @@ int main( int argc, char *argv[] ) {
    clilen = sizeof(cli_addr);
    
    /* Accept actual connection from the client */
-   while(newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+   newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 	
    if (newsockfd < 0) {
       perror("ERROR on accept");
@@ -71,7 +71,7 @@ int main( int argc, char *argv[] ) {
    
    
    printf("Here is the message: %s\n",strH);
-
+	close(newsockfd);
 	close(sockfd);
    return 0;
 }
